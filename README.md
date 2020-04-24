a35fd6f23d3fa4e15a6b38caf45a74ab07a03a4d
Denis Onder <denis_onder@protonmail.com>
2020-04-25 01:43:36 +0200

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# ebook-reader
+
+Simple Go back-end for serving your PDF e-books
---
+++ src/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func startServer() {
+	p, exists := os.LookupEnv("PORT")
+
+	if !exists {
+		fmt.Println("The port has not been set.")
+		os.Exit(1)
+	}
+
+	port := ":" + p
+
+	dir := http.Dir("../ebooks")
+	fs := http.FileServer(dir)
+	http.Handle("/", fs)
+
+	fmt.Println("Listening on port " + port)
+
+	err := http.ListenAndServe(port, nil)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
